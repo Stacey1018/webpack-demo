cc49aaaccbdde50669212bea2a666e5f675b9fe8
zhangyang <zhangyang@inke.cn>
2022-03-27 17:04:22 +0800

    配置htmlwebapckplguin cleanwebpackplugin

--- webpack.base.js
+++ webpack.base.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -17,6 +19,7 @@ module.exports = {
     ],
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({ title: 'demo' })],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
